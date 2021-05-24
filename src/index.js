077aa7412cc53159a2f47e4a91989e05b8927b49
ganeshInnoctive <ganesh@innoctive.com>
2021-05-24 18:06:31 +0530

    Pushing completed code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
+class App extends React.Component {
+    state = { lat: null, long: null, errorMessage: '' }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({
+                lat: position.coords.latitude,
+                long: position.coords.longitude
+            }),
+            (error) => this.setState({
+                errorMessage: error.message
+            })
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        )
+    }
+
+    renderContent() {
+        // Show error
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        // Show data
+        if (!this.state.errorMessage && this.state.lat && this.state.long) {
+            return <SeasonDisplay lat={this.state.lat} long={this.state.long} />
+        }
+
+
+        return <Spinner message="Please accept location request" />
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
+if (module.hot) {
+    module.hot.accept()
+}
